5573de38b4f29f3d94303ca4fc285dbd22305f96
HanSeungmin <himmel-aufstieg@nate.com>
2015-12-04 05:41:45 +0900

    Exam_Equip

---
+++ Item_Equip/Assets/ItemEquip.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemEquip : MonoBehaviour {
+
+    public GameObject ItemPrefeb;
+
+    GameObject Item;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!Item)
+            {
+                Item = Instantiate(ItemPrefeb);
+            }
+            else if (Item)
+            {
+                Destroy(Item);
+            }
+        }
+	
+	}
+}
